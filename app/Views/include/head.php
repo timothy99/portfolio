19ecd40b5eac0a66bcbeffc240d0cdca98a6d457
timothy99 <joydisk@gmail.com>
2021-03-22 17:07:50 +0900

    문구 수정

--- app/Views/include/head.php
+++ app/Views/include/head.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>K-비대면바우처플랫폼</title>
+	<title>포트폴리오</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1"><!-- Tell the browser to be responsive to screen width -->
 
--- app/Views/include/left.php
+++ app/Views/include/left.php
@@ -9,7 +9,7 @@
 	<aside class="main-sidebar sidebar-dark-primary elevation-4"><!-- Main Sidebar Container -->
 		<a href="/" class="brand-link"><!-- Brand Logo -->
 			<img src="/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-			<span class="brand-text font-weight-light">관리자</span>
+			<span class="brand-text font-weight-light">포트폴리오</span>
 		</a>
 
 		<div class="sidebar"><!-- Sidebar -->
@@ -34,6 +34,28 @@
 								<a href="/business/businessSearch" class="nav-link <?php if($segments[0] == "business" && $segments[1] == "businessSearch") echo "active"; ?>">
 									<i class="far fa-circle nav-icon"></i>
 									<p>휴폐업조회</p>
+								</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+				<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+					<li class="nav-item has-treeview <?php if($segments[0] == "messenger") echo "menu-open"; ?>">
+						<a href="#" class="nav-link <?php if($segments[0] == "messenger") echo "active"; ?>">
+							<i class="nav-icon fas fa-chart-pie"></i>
+							<p>메신저<i class="right fas fa-angle-left"></i></p>
+						</a>
+						<ul class="nav nav-treeview">
+							<li class="nav-item">
+								<a href="/messenger/slack" class="nav-link <?php if($segments[0] == "messenger" && $segments[1] == "slack") echo "active"; ?>">
+									<i class="far fa-circle nav-icon"></i>
+									<p>슬랙</p>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a href="/messenger/telegram" class="nav-link <?php if($segments[0] == "messenger" && $segments[1] == "telegram") echo "active"; ?>">
+									<i class="far fa-circle nav-icon"></i>
+									<p>텔레그램</p>
 								</a>
 							</li>
 						</ul>
